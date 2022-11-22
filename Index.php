a1004807a3ba26713234f146db35b444daffc005
FilipAkbyr <rybkafilip@seznam.cz>
2022-11-22 13:59:32 +0100

    ...

--- Index.php
+++ Index.php
@@ -19,6 +19,7 @@ $charset = $config['charset'];
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
+
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
